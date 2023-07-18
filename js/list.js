53f8d0836d3a06f9bca9352ad130afcf1975b21e
Rute-r <rute_ke@hotmail.com>
2023-07-18 15:40:50 -0300

    localStorage

--- js/list.js
+++ js/list.js
@@ -2,6 +2,10 @@ const form = document.querySelector('#newItem');
 const list = document.querySelector('#list');
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
+items.forEach((element) => {
+  createItem(element);
+});
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
@@ -9,7 +13,16 @@ form.addEventListener('submit', (event) => {
   const amount = event.target.elements['amount'];
   const itemMeasure = event.target.elements['itemMeasure'];
 
-  createItem(name.value, amount.value, itemMeasure.value);
+  const currentItem = {
+    name: name.value,
+    amount: amount.value,
+    itemMeasure: itemMeasure.value,
+  };
+
+  createItem(currentItem);
+  items.push(currentItem);
+
+  localStorage.setItem('items', JSON.stringify(items));
 
   name.value = '';
   amount.value = '';
@@ -17,15 +30,15 @@ form.addEventListener('submit', (event) => {
   document.querySelector('#name').focus();
 });
 
-function createItem(name, amount, itemMeasure) {
+function createItem(item) {
   const itemError = document.querySelector('.item-error');
   const amountError = document.querySelector('.amount-error');
 
-  if (name === '') {
+  if (item.name === '') {
     itemError.innerHTML = 'Preencha com o item desejado!';
     itemError.style.display = 'block';
     amountError.style.display = 'none';
-  } else if (amount === '') {
+  } else if (item.amount === '') {
     amountError.innerHTML = 'Preencha com a quantidade desejada!';
     amountError.style.display = 'block';
     itemError.style.display = 'none';
@@ -37,29 +50,36 @@ function createItem(name, amount, itemMeasure) {
     newItem.classList.add('listItem');
 
     const numberItem = document.createElement('strong');
-    numberItem.innerHTML = amount + itemMeasure;
-
+    numberItem.innerHTML = item.amount + item.itemMeasure;
+    numberItem.dataset.id = item.id;
     newItem.appendChild(numberItem);
-    newItem.innerHTML += ' - ' + name;
-    newItem.appendChild(deleteBtn());
+    newItem.innerHTML += ' - ' + item.name;
+    newItem.appendChild(deleteBtn(item.id));
 
     list.appendChild(newItem);
   }
 }
 
-function deleteBtn() {
+function deleteBtn(id) {
   const elementButton = document.createElement('button');
   elementButton.classList.add('deleteButton');
 
   elementButton.innerText = 'X';
 
   elementButton.addEventListener('click', function () {
-    deleteElement(this.parentNode);
+    deleteElement(this.parentNode, id);
   });
 
   return elementButton;
 }
 
-function deleteElement(tag) {
+function deleteElement(tag, id) {
   tag.remove();
+
+  items.splice(
+    items.findIndex((element) => element.id === id),
+    1
+  );
+
+  localStorage.setItem('items', JSON.stringify(items));
 }
